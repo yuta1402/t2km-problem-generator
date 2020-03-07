f6c96bd003941bae62d6335c076f67568c062943
gut <yuta1402souja@live.jp>
2020-03-08 01:00:37 +0900

    Remove some dead code

--- main.go
+++ main.go
@@ -6,36 +6,10 @@ import (
 	"os"
 	"time"
 
-	"github.com/sclevine/agouti"
 	"github.com/yuta1402/t2km-problem-generator/problems"
 )
 
 func main() {
-	capabilities := agouti.NewCapabilities()
-	capabilities["phantomjs.page.settings.userAgent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"
-	options := agouti.Desired(capabilities)
-
-	driver := agouti.PhantomJS(options)
-	defer driver.Stop()
-	driver.Start()
-
-	page, err := driver.NewPage()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
-	}
-
-	page.Navigate("https://example.com/")
-
-	time.Sleep(1 * time.Second)
-
-	src, err := page.HTML()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
-	}
-	fmt.Println(src)
-
 	problems, err := problems.New()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
