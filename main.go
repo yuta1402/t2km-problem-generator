73a033c6a984f02c5112c79486b35dd3de49fa7a
gut <yuta1402souja@live.jp>
2020-03-08 15:14:36 +0900

    Add subcommand to list

--- main.go
+++ main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/yuta1402/t2km-problem-generator/problems"
 )
 
+func parsePoints(pointsStr string) ([]float64, error) {
+	slice := strings.Split(pointsStr, "-")
+
+	points := []float64{}
+
+	for _, s := range slice {
+		p, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		points = append(points, p)
+	}
+
+	return points, nil
+}
+
 func main() {
-	problems, err := problems.New()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+	rand.Seed(time.Now().UnixNano())
+
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+
+	if len(os.Args) == 1 {
+		flag.Usage()
 		return
 	}
 
-	fmt.Println(len(problems))
+	switch os.Args[1] {
+	case "list":
+		pointsStr := listCmd.String("points", "", "problem points (e.g. 100-200-300-400)")
+		listCmd.Parse(os.Args[2:])
 
-	rand.Seed(time.Now().UnixNano())
+		fmt.Println(*pointsStr)
+		points, err := parsePoints(*pointsStr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "parse points error: %s\n", err)
+			return
+		}
 
-	probs := problems.RandomSelectByPoints([]float64{100, 200, 300, 400, 500})
-	fmt.Println(probs)
+		problems, err := problems.New()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			return
+		}
+		probs := problems.RandomSelectByPoints(points)
+		fmt.Println(probs)
+	}
 }
