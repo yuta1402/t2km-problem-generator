344818873a067934035c08dfa32e010b8575679a
gut <yuta1402souja@live.jp>
2020-03-13 01:07:04 +0900

    Support contest name prefix

--- main.go
+++ main.go
@@ -102,6 +102,7 @@ func main() {
 	var (
 		id           string
 		password     string
+		namePrefix   string
 		pointsStr    string
 		startWeekday int
 		startTimeStr string
@@ -112,6 +113,7 @@ func main() {
 
 	flag.StringVar(&id, "id", "", "id of atcoder virtual contest")
 	flag.StringVar(&password, "password", "", "password of atcoder virtual contest")
+	flag.StringVar(&namePrefix, "name-prefix", "tmp contest", "prefix of contest name")
 	flag.StringVar(&pointsStr, "points", "100-200-300-400", "problem points")
 	flag.IntVar(&startWeekday, "start-weekday", int(now.Weekday()), "start weekday Sun=0, Mon=1, ... (default now.Weekday())")
 	flag.StringVar(&startTimeStr, "start-time", "21:00", "start time")
@@ -163,7 +165,7 @@ func main() {
 	}
 
 	option := contest.Option{
-		NamePrefix:  "tmp contest",
+		NamePrefix:  namePrefix,
 		Description: "",
 		StartTime:   startTime,
 		DurationMin: time.Duration(durationMin) * time.Minute,
